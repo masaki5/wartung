172b7ba4009f8e3be48e844548f07c2996086551
masaki5 <xxtmhxx5@gmail.com>
2020-02-12 10:56:11 +0000

    a

--- db/migrate/20200205081417_create_cars.rb
+++ db/migrate/20200205081417_create_cars.rb
@@ -2,6 +2,9 @@ class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
       t.string :name
+      t.integer :mileage
+      t.date :register
+      t.date :inspection
       t.string :user_id
 
       t.timestamps
